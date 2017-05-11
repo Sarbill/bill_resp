a39490b27e297e47fedf8977baaaaa9190574c50
Sarbill <719943546@qq.com>
2017-05-11 15:11:27 +0800

    消息持久化

--- rabbit-hello/src/main/java/work_queue/NewTask.java
+++ rabbit-hello/src/main/java/work_queue/NewTask.java
@@ -3,6 +3,7 @@ package work_queue;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -23,13 +24,14 @@ public class NewTask {
 
         try {
             Channel channel=getChannel();
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            boolean durable=true;//消息持久化
+            channel.queueDeclare(QUEUE_NAME, durable, false, false, null);//设置durable为ture,则rabbitmq重启后不会丢失队列，可以理解为持久化队列
             StringBuilder message=new StringBuilder("hello,world");
 
             for (int i=0;i<10;i++){
                 message.append(".");
                 System.out.println("发送第"+(i+1)+"条消息："+message.toString());
-                channel.basicPublish("", QUEUE_NAME, null, message.toString().getBytes("UTF-8"));
+                channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.toString().getBytes("UTF-8"));//必须将消息内容也持久化
             }
 
 
